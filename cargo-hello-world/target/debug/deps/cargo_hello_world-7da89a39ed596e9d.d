2108baa23846348822ff5c7ca5b63d9d2d3ad10f
Muntasir Raihan Rahman <mrahman@keystonestrategy.com>
2025-08-29 06:32:13 -0700

    add basic cargo

---
+++ cargo-hello-world/target/debug/deps/cargo_hello_world-7da89a39ed596e9d.d
@@ -0,0 +1,5 @@
+/Users/muntasirraihanrahman/Projects/rust_projects/cargo-hello-world/target/debug/deps/cargo_hello_world-7da89a39ed596e9d.d: src/main.rs
+
+/Users/muntasirraihanrahman/Projects/rust_projects/cargo-hello-world/target/debug/deps/cargo_hello_world-7da89a39ed596e9d: src/main.rs
+
+src/main.rs:
